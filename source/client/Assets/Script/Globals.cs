acd6f53c1f0a47db21c41fde0ce70e2fbfbd16c8
王宏亮 <whl0070179@163.com>
2015-03-02 15:44:38 +0800

    [王宏亮]倒塌 结构

---
+++ source/client/Assets/Script/Globals.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals : MonoBehaviour
+{
+    private StateMng m_stateMng = null;
+    private EntityMng m_entityMng = null;
+    private SceneMng m_sceneMng= null;
+    private CameraController m_cameraController = null;
+    private TimeMng m_timeMng = null;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    T _CreateLink<T>() where T : Component
+    {
+        T link = this.GetComponentInChildren<T>();
+        if (link == null)
+        {
+            GameObject linkObj = new GameObject(typeof(T).Name);
+            link = linkObj.AddComponent<T>();
+            linkObj.transform.parent = this.transform;
+        }
+
+        return link;
+    }
+}
